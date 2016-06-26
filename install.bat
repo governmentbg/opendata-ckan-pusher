8aa91890316898f975b0bb28c300004683a501e3
bozhanov <bozhidar.bozhanov@tomtom.com>
2016-06-26 14:20:31 +0300

    fixing installer paths

--- install.bat
+++ install.bat
@@ -1,9 +1,9 @@
 prunsrv //IS//OpenDataPusher --DisplayName="OpenData Pusher" --Description="OpenData Pusher"^
-     --Install="%cd%\prunsrv.exe" --Jvm="%cd%\jre1.8.0_91\bin\server\jvm.dll" --StartMode=jvm --StopMode=jvm^
+     --Install="%cd%\prunsrv.exe" --Jvm="%cd%\jre1.8.0_91\bin\client\jvm.dll" --StartMode=jvm --StopMode=jvm^
      --Startup=auto --StartClass=bg.government.opendatapusher.Pusher --StopClass=bg.government.opendatapusher.Pusher^
      --StartParams=start --StopParams=stop --StartMethod=windowsService --StopMethod=windowsService^
-     --Classpath="%cd%\opendata-ckan-pusher.jar" --LogLevel=DEBUG^ --LogPath=%cd%\logs --LogPrefix=procrun.log^
-     --StdOutput=%cd%\logs\stdout.log --StdError=%cd%\logs\stderr.log
+     --Classpath="%cd%\opendata-ckan-pusher.jar" --LogLevel=DEBUG^ --LogPath="%cd%\logs" --LogPrefix=procrun.log^
+     --StdOutput="%cd%\logs\stdout.log" --StdError="%cd%\logs\stderr.log"
      
      
 prunsrv //ES//OpenDataPusher
